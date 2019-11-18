5bcfa2103af95a18c88f0db20a856e255ea4cad5
ravikumar-j <ravikumarjangapally1@gmail.com>
2019-11-18 16:42:19 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './account/accounts.service';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ AccountsService]
+})
+export class AppComponent implements OnInit {
+  accounts: { name: string, status : string }[]=[];
+
+  constructor(private accountsService: AccountsService){}
+ 
+  ngOnInit (){
+      this.accounts = this.accountsService.accounts;
+  }
+
+}
+  // accounts = [
+  //   {
+  //     name: 'Master Account',
+  //     status: 'active'
+  //   },
+  //   {
+  //     name: 'Testaccount',
+  //     status: 'inactive'
+  //   },
+  //   {
+  //     name: 'Hidden Account',
+  //     status: 'unknown'
+  //   }
+  // ];
+
+  // onAccountAdded(newAccount: {name: string, status: string}) {
+  //   this.accounts.push(newAccount);
+  // }
+
+  // onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+  //   this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  // }
+
